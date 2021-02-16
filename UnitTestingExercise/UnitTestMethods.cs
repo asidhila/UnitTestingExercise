d2f0870d07862af1c83ccae31c3ab0a9657a3bc8
anjaliesidhilall@gmail.com <anjaliesidhilall@gmail.com>
2021-02-16 13:09:33 -0500

    Unit exercise done

--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -3,24 +3,44 @@ namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        //DONE - Make sure your references are set properly
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
-        // Create an Add method that passes 3 integers
+        //DONE- Create an Add method that passes 3 integers
+        public int Add(int one, int two, int three)
+        {
+            return one + two + three;
+        }
 
-        // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        //DONE - Create a Subtract method that passes 2 integers
+        public int Subtract(int minuend, int subtrhend)
+        {
+            return minuend - subtrhend;
+        }
 
-        // Create a Multiply method that passes 2 integers
-
-        // Create a Divide method that passes 2 integers
-
-
-
+        // DONE - Create a Multiply method that passes 2 integers
+        public int Multiply(int one, int two)
+        {
+            throw new NotImplementedException();
+        }
+        //Done -  Create a Divide method that passes 2 integers
+        public int Divide(int one, int two)
+        {
+            throw new NotImplementedException();
+        }
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public char ReturnH()
+        {
+            return 'h';
+        }
+
+    }
+
+}
 
         
-    }
-}
+    
+
+
